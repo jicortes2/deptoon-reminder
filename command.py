53864b7050b2f8b8d08e553c533b511a89c3275d
Juan Cortes <jicortes2@uc.cl>
2018-09-22 00:42:53 -0300

    :construction: testing method

--- command.py
+++ command.py
@@ -4,15 +4,29 @@ import os
 
 
 TOKEN = str(os.environ['TOKEN'])
+TYPES = {
+    'once': 0,
+    'monthly': 1,
+    'annually': 2
+}
 # Public commands
 
 def start(sender_id, command):
     return "Deptoon Reminder at your service"
 
 def reminder(chat_id, command, args):
-    print(command)
-    print(args)
-    return command
+    """
+        Add a reminder to your chat
+        Use: /reminder periodicity date message
+        Example: /reminder monthly 22-11 This is an example
+        Periodicity options: once, monthly, annually
+    """
+    # /reminder once 23-06 hola!
+    if args.count(' ') < 3:
+        return reminder.__doc__
+    frequency, date, msg = args.split(' ', 2)
+    periodicity = TYPES[frequency.lower()]
+    return args
 
 # Auxiliary methods
 
