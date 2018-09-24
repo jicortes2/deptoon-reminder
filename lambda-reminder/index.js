6d02490f7396dbba15d21c28dbe6b90a04fe8a89
Juan Cortes <jicortes2@uc.cl>
2018-09-24 16:01:13 -0300

    :tada: :package: updated lambda only to make post

--- lambda-reminder/index.js
+++ lambda-reminder/index.js
@@ -1,32 +1,10 @@
 const axios = require("axios")
 const URL = process.env.BASE_URL + process.env.TOKEN
 
-function headers() {
-  return {
-    "Content-Type": "application/json"
-  }
-}
-
-function dateMessage() {
-  const today = new Date();
-  const date = today.getDate();
-  let message = "Hola! Se acerca la fecha de pagar las siguientes cuentas:\n";
-  if (date === 1 || date === 3 || date === 5 || true) {
-    message += "- Universidad https://www12.uc.cl/MediosDePagos/jsp/mdp_inicio.jsp"
-    message += "\n  Rut Juan: 18.586.535-5\n  Rut Cristian: 18.586.536-3\n"
-  }
-  return JSON.stringify({
-    message: message,
-    statusCode: 200
-  });
-}
-
 exports.handler = (event, context, callback) => {
-    const msg = dateMessage();
-    const headers = JSON.stringify({headers: headers()});
-    axios.post(URL, {"data": msg}, headers)
+    axios.post(URL, {}, {})
     .then((response) => {
-      callback(null, msg);
+      callback(null, "OK");
     }).catch((e) => {
       callback(e);
     });
